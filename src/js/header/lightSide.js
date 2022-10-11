05b8dfab1ce29db7b19254cde53db5be73901560
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-10-12 02:07:30 +0300

    sum

--- src/js/header/lightSide.js
+++ src/js/header/lightSide.js
@@ -4,8 +4,8 @@ const buttonText = document.querySelector('.settings__text');
 
 buttonRef.addEventListener('click', () => {
   spanEl.forEach(element => {
-    if (element.style.cssText !== 'background-color: brown;') {
-      element.setAttribute('style', 'background-color: ' + 'brown');
+    if (element.style.cssText !== 'background-color: black;') {
+      element.setAttribute('style', 'background-color: ' + 'black');
       buttonText.textContent = 'LIGHT SIDE';
     } else {
       element.setAttribute('style', 'background-color: ' + '#fff');
