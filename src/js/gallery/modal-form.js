140a5383a08b9edbfb40361d4e3567c14706c340
Ruslan Negoda <iarjuna108@gmail.com>
2022-10-16 15:59:37 +0300

    121212

--- src/js/gallery/modal-form.js
+++ src/js/gallery/modal-form.js
@@ -1,5 +1,6 @@
 // import API_KEY from '../api/apiKey';
 import { load, save, remove } from './local-st-load-remove-save';
+import onTrailerClick from './trailer';
 
 const galleryFilm = document.querySelector('.cards__list--home');
 const modalEl = document.querySelector('.modal');
@@ -296,7 +297,8 @@ function murckupCard({
   id,
   backdrop_path,
 }) {
-  return (modalEl.innerHTML = `
+  return (
+    (modalEl.innerHTML = `
   <div class='modal__backdrop'
               style="background-image:linear-gradient(to right, rgba(47, 48, 58, 0.4), rgba(47, 48, 58, 0.4)),
               url('https://image.tmdb.org/t/p/original/${backdrop_path}');
@@ -312,6 +314,16 @@ function murckupCard({
         title=''
         width='336'
       />
+       <button class='btn-trailer' type='button' aria-label='play movie trailer'>
+      <svg class='btn-trailer__svg' width='68' height='48' viewBox='0 0 68 48'>
+        <path
+          class='btn-trailer__path'
+          d='M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z'
+          fill='#212121'
+        ></path>
+        <path d='M 45,24 27,14 27,34' fill='#fff'></path>
+      </svg>
+    </button>
     </div>
 
     <div class='film__information'>
@@ -360,7 +372,9 @@ function murckupCard({
     </button>
     </div>
   </div>
-  `);
+  `),
+    onTrailerClick()
+  );
 }
 
 // --------------------------------------------
